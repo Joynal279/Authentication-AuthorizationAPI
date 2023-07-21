6a5c7ec3433d06b8ed98bd2d3fd971199e3f2f10
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-07-21 13:30:43 +0600

    implemented login method

--- Authentication&AuthorizationAPI/Controllers/AuthController.cs
+++ Authentication&AuthorizationAPI/Controllers/AuthController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Authentication_AuthorizationAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,34 @@ namespace Authentication_AuthorizationAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        public static User user = new User();
+
+        //Register method
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            user.UserName = request.UserName;
+            user.PasswordHash = passwordHash;
+
+            return Ok(user);
+        }
+
+        //Login method
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request)
+        {
+            if (user.UserName != request.UserName)
+            {
+                return BadRequest("User not found");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password");
+            }
+
+            return Ok(user);
+        }
     }
 }
