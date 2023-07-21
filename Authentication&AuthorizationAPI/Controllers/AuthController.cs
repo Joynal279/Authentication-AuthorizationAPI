6ce52d4d9a40270dae2d961066a75ffa65eb055a
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-07-21 18:18:05 +0600

    CreateToken method implemented

--- Authentication&AuthorizationAPI/Controllers/AuthController.cs
+++ Authentication&AuthorizationAPI/Controllers/AuthController.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Authentication_AuthorizationAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Authentication_AuthorizationAPI.Controllers
 {
@@ -13,6 +17,12 @@ namespace Authentication_AuthorizationAPI.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         //Register method
         [HttpPost("register")]
@@ -39,7 +49,29 @@ namespace Authentication_AuthorizationAPI.Controllers
                 return BadRequest("Wrong password");
             }
 
-            return Ok(user);
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+            var token = new JwtSecurityToken(
+                        claims: claims,
+                        expires: DateTime.Now.AddDays(1),
+                        signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
